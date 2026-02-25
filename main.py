efc8e044ed4d41bc919d80e0291fac153980c817
Arunima-2232 <145280505+Arunima-2232@users.noreply.github.com>
2026-02-25 19:15:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+from os import path
+import speech_recognition as sr
+import sys
+import whisper
+import warnings
+import time
+import pyautogui
+import webbrowser
+import os
+import pyttsx3
+engine=pyttsx3.init()
+
+base_model_path=os.path.expanduser('~/.cache/whisper/base.pt')
+base_model=whisper.load_model(base_model_path)
+r=sr.Recognizer()
+source=sr.AudioFile("harvard.wav")
+
+def listen_for_command():
+
+    with source as s:
+        print("listening for command...")
+        r.adjust_for_ambient_noise(s)
+        audio=r.listen(s)
+
+    try:
+        command=base_model.transcribe("harvard.wav")
+        if command and command['text']:
+            return command['text'].lower()
+        return None
+    except sr.UnknownValueError:
+        return "could not understand"
+    except sr.RequestError as e:
+        return "error {0}".format(e)
+
+def respond(text):
+    if sys.platform!='darwin':
+        engine.say(text)
+        engine.runAndWait()
+    
+
+if __name__=="__main__":
+    textToSay=listen_for_command()
+    respond(textToSay)
